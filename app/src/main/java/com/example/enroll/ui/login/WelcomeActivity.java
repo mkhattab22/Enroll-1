6dc4ae9122a6c01e897ff1e8d932320ea8d3a77f
alexclements74 <aclem105@uottawa.ca>
2022-10-31 13:09:26 -0400

    Updating Github to Latest Project Version

---
+++ app/src/main/java/com/example/enroll/ui/login/WelcomeActivity.java
@@ -0,0 +1,60 @@
+package com.example.enroll.ui.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.enroll.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+    Button con;
+    MyDBHandler db = new MyDBHandler(this);
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String name = "";
+        String account_type = "";
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            name = extras.getString("KEY");
+            account_type = db.checkType(name);
+        }
+
+        con = (Button) findViewById(R.id.con);
+
+//        Intent i = getIntent();
+//        Bundle values = i.getExtras();
+//        String user = i.getStringExtra("KEY1");
+//        //line that causes error
+//        //String account_type = values.getString("KEY2");
+
+        TextView t = (TextView) findViewById(R.id.welcome);
+        t.setText("Welcome " + name + "!, you are logged in as " + account_type );
+        con.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getApplicationContext(), Enrol.class);
+
+                startActivity(myIntent);
+
+            }
+        });
+
+
+    }
+}
+
